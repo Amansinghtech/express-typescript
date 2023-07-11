d36bb43ec33d89be0f84bd833e4bdae0abb6810e
Aman Singh <singhaman11415@gmail.com>
2023-07-11 20:46:26 +0530

    changed uri

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,11 @@
 import express from 'express'
 import UsersRoute from './routers/users'
 import mongoose from 'mongoose'
-import users from './models/users'
 
 const app = express()
 const port = 3000
 app.use(express.json())
-const mongoURI =
-	'mongodb+srv://dbadmin:7RjWEqA8tfYfEwfa@cluster0.zvuk7wj.mongodb.net/?retryWrites=true&w=majority'
+const mongoURI = process.env.MONGO_URI
 
 // use the users route
 app.use('/users', UsersRoute)
