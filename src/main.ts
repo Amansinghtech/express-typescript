897fb99ae27b2813e9dc7c857b626be229b47ab8
Aman Singh <singhaman11415@gmail.com>
2023-07-11 20:01:59 +0530

    added mongoose

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import express from 'express'
 import UsersRoute from './routers/users'
+import mongoose from 'mongoose'
+import users from './models/users'
 
 const app = express()
 const port = 3000
-
 app.use(express.json())
+const mongoURI =
+	'mongodb+srv://dbadmin:7RjWEqA8tfYfEwfa@cluster0.zvuk7wj.mongodb.net/?retryWrites=true&w=majority'
 
 // use the users route
 app.use('/users', UsersRoute)
@@ -15,9 +18,18 @@ app.route('/').get((req, res) => {
 })
 
 function main() {
-	app.listen(port, () => {
-		console.log(`listening at http://localhost:${port}`)
-	})
+	// connect to mongodb
+	mongoose
+		.connect(mongoURI)
+		.then(() => {
+			console.log('connected to mongodb')
+			app.listen(port, () => {
+				console.log(`listening at http://localhost:${port}`)
+			})
+		})
+		.catch((err) => {
+			console.log(err)
+		})
 }
 
 main()
