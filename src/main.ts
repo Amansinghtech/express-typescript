79f541f9dd628a829932b18ad95859a892721e40
Aman Singh <singhaman11415@gmail.com>
2023-07-11 20:50:38 +0530

    commit message

---
+++ src/main.ts
@@ -0,0 +1,33 @@
+import express from 'express'
+import UsersRoute from './routers/users'
+import mongoose from 'mongoose'
+
+const app = express()
+const port = 3000
+app.use(express.json())
+const mongoURI = process.env.MONGO_URI
+
+// use the users route
+app.use('/users', UsersRoute)
+
+// hello world route for root
+app.route('/').get((req, res) => {
+	res.send('hello world')
+})
+
+function main() {
+	// connect to mongodb
+	mongoose
+		.connect(mongoURI)
+		.then(() => {
+			console.log('connected to mongodb')
+			app.listen(port, () => {
+				console.log(`listening at http://localhost:${port}`)
+			})
+		})
+		.catch((err) => {
+			console.log(err)
+		})
+}
+
+main()
---
+++ src/models/users.ts
@@ -0,0 +1,19 @@
+import mongoose, { Schema, Document } from 'mongoose'
+
+interface User extends Document {
+	name: string
+	username: string
+	email: string
+	password: string
+}
+
+const UserSchema = new Schema<User>({
+	name: { type: String },
+	username: { type: String, required: true },
+	email: {
+		type: String,
+	},
+	password: { type: String, required: true },
+})
+
+export default mongoose.model<User>('User', UserSchema)
