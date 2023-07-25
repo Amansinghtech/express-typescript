0cc13fc727cab80784f758044df585ad7b143ce7
Aman Singh <singhaman11415@gmail.com>
2023-07-25 12:37:31 +0530

    added users model

--- src/models/users.ts
+++ src/models/users.ts
@@ -1,14 +1,27 @@
 import mongoose, { Schema, Document } from 'mongoose'
+import { v4 } from 'uuid'
+enum Gender {
+	male = 'MALE',
+	female = 'FEMALE',
+	other = 'OTHER',
+}
 
 export interface User extends Document {
-	fullname: string
+	uid: string
 	email: string
+	fullname: string
+	bio: string
 	password: string
 	phone: string
+	dialCode: string
+	age: number
+	gender: Gender
 }
 
 const UserSchema = new Schema<User>({
+	uid: { type: String, unique: true, required: true, default: v4 },
 	fullname: { type: String },
+	bio: { type: String },
 	email: {
 		type: String,
 		unique: true,
@@ -19,6 +32,20 @@ const UserSchema = new Schema<User>({
 	},
 	phone: {
 		type: String,
+	},
+	dialCode: {
+		type: String,
+	},
+	age: {
+		type: Number,
+		min: 1,
+		max: 100,
+	},
+
+	gender: {
+		type: String,
+		enum: Object.values(Gender),
+		default: null,
 	},
 })
 
