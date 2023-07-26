ef98f6bcb528dded98afd328d4412c734e64a3d1
Hdewani <1320203902@bitraipur.ac.in>
2023-07-26 19:04:33 +0530

    added model for follow request

---
+++ src/models/followrequest.ts
@@ -0,0 +1,24 @@
+import mongoose, { Schema, Document, ObjectId } from 'mongoose'
+
+
+
+export interface FollowRequest extends Document {
+    followedUser: ObjectId
+    createdOn:Date
+    accepted:Boolean
+    unfollowed:Boolean
+    followedBy:String[]
+}
+
+const FollowRequestSchema = new Schema({
+    followedUser: {type: Schema.Types.ObjectId, ref: 'user'},
+    createdOn: {type: Date, default: Date.now},
+    accepted: { type: Boolean,default: true  },
+    unfollowed: { type: Boolean ,default: true  },
+    followedBy: { type: [String], default: [] },
+
+})
+
+FollowRequestSchema.index({ user: 1 })
+
+export default mongoose.model<FollowRequest>('FollowRequest', FollowRequestSchema)
