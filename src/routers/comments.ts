3dfc819b4ce0f6a4b550d07275480ad56f4995c3
Harshv1209 <1320204019@bitraipur.ac.in>
2023-08-06 19:55:57 +0530

    updated addComment ,updateComment,getComment,deleteComment api

--- src/routers/comments.ts
+++ src/routers/comments.ts
@@ -13,8 +13,19 @@ const addCommentSchema = z.object({
   comment: z.string().min(1).max(1000),
   tags: z.array(z.string().max(50)).max(10).optional(),
 })
+const updateCommentSchema = z.object({
+  comment: z.string().min(1).max(1000).optional(),
+  tags: z.array(z.string().max(50)).max(10).optional(),
+})
+const getCommentSchema = z.object({
+  limit: z.coerce.number().min(0).max(100).default(10).optional(),
+  skip: z.coerce.number().min(0).default(0).optional(),
+  sortOrder: z.enum(["asc", "desc"]).default("asc").optional(),
+})
 
 type addCommentInput = z.infer<typeof addCommentSchema>
+type updateCommentInput = z.infer<typeof updateCommentSchema>
+type getCommentInput = z.infer<typeof getCommentSchema>
 
 const validateaddComment: RequestHandler = (req, res, next) => {
   try {
@@ -24,11 +35,27 @@ const validateaddComment: RequestHandler = (req, res, next) => {
     return res.status(400).json(error)
   }
 }
+const validateupdateComment: RequestHandler = (req, res, next) => {
+  try {
+    updateCommentSchema.parse(req.body)
+    next()
+  } catch (error) {
+    return res.status(400).json(error)
+  }
+}
+const validategetComment: RequestHandler = (req, res, next) => {
+  try {
+    updateCommentSchema.parse(req.body)
+    next()
+  } catch (error) {
+    return res.status(400).json(error)
+  }
+}
 interface PopuplatedComment extends Omit<comments, "post" | "commentedBy"> {
   post: posts
   commentedBy: User
 }
-
+// add comment
 router.post(
   "/addComment/:id",
   validateaddComment,
@@ -36,9 +63,9 @@ router.post(
     try {
       console.log("created comment successfully")
       const { comment, tags } = req.body as addCommentInput
-      const post = (await PostModal.findOne({
+      const post: PopuplatedComment = await PostModal.findOne({
         id: req.params.id,
-      })) as PopuplatedComment
+      })
 
       console.log(post)
 
@@ -52,6 +79,7 @@ router.post(
         message: "added comment successfully",
         payload: {
           commentedBy: res.locals.user._id,
+          post: post._id,
           createdOn: newComment.createdOn,
           editedOn: newComment.editedOn,
           visibility: newComment.visibility,
@@ -65,11 +93,71 @@ router.post(
     }
   }
 )
-// delete comment
-router.delete("deleteComment/:id", async (req, res) => {
-  console.log("harshall comment")
+// update comment
+router.put("/updateComment/:id", validateupdateComment, async (req, res) => {
   try {
-    // console.log("harsh")
+    const commentId = req.params.id
+
+    const { comment, tags } = req.body as updateCommentInput
+
+    const updatedComment: PopuplatedComment =
+      await CommentModal.findOneAndUpdate(
+        { _id: commentId },
+        {
+          $set: {
+            comment: comment,
+            tags: tags,
+          },
+        },
+        {
+          new: true,
+        }
+      ).populate(["post", "commentedBy"])
+
+    if (!updatedComment) {
+      return res.status(404).json({ message: "Comment not found" })
+    }
+
+    console.log("Updated comment successfully")
+
+    return res.status(200).json({
+      message: "Comment updated",
+      payload: {
+        comment: updatedComment.comment,
+        tags: updatedComment.tags,
+      },
+    })
+  } catch (error) {
+    console.log("Error:", error)
+    return res.status(500).json({ message: "Internal server error" })
+  }
+})
+
+router.get("/getComments/:id", validategetComment, async (req, res) => {
+  try {
+    const { limit, skip, sortOrder } = req.query as getCommentInput
+
+    const comments: PopuplatedComment = await CommentModal.find({
+      post: req.params.id,
+    })
+      .populate(["comment", "tags"])
+      .sort({ createdOn: sortOrder })
+      .limit(limit)
+      .skip(skip)
+      .lean()
+
+    return res.status(200).json({
+      message: "Fetched comments successfully",
+      payload: comments,
+    })
+  } catch (error) {
+    console.error(error)
+    return res.status(500).json({ message: "Internal server error" })
+  }
+})
+// delete comment
+router.delete("/deleteComment/:id", async (req, res) => {
+  try {
     const post = await CommentModal.findOneAndDelete({
       _id: req.params.id,
     })
