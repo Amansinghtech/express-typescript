33520e8d8bbd73d414aa762697dd6208713bb061
Aman Singh <singhaman11415@gmail.com>
2023-08-11 08:09:11 +0530

    added getFeeds route and fixed minor bugs

--- src/models/posts.ts
+++ src/models/posts.ts
@@ -5,6 +5,7 @@ export interface posts extends Document {
 	id: string
 	user: ObjectId
 	caption: string
+	body: string
 	tags: string[]
 	createdOn: Date
 	lastEdited: Date
@@ -14,6 +15,7 @@ const postsSchema = new Schema({
 	id: { type: String, unique: true, required: true, default: v4 },
 	user: { type: Schema.Types.ObjectId, ref: 'users', required: true },
 	caption: { type: String, required: true },
+	body: { type: String, default: null },
 	tags: { type: [String], default: null },
 	createdOn: { type: Date, default: Date.now, required: true },
 	lastEdited: { type: Date, default: null },
